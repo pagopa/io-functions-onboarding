828b6e74be2a1436db8b3447f3debe3bdef7ff1d
Alessandro Ercolani <ale.ercolani@gmail.com>
2020-01-19 23:25:05 +0100

    Timer and durable functions for attachments and verify signatures

---
+++ VerifyAttachmentsActivity/index.ts
@@ -0,0 +1,36 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ *
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+import { AzureFunction, Context } from "@azure/functions";
+import * as U from "../commons/verify-utils/utils";
+
+const verifyAttachments = () =>
+  U.verifyAllAttachments.map(
+    taskEmails => taskEmails.run()
+    // only for showing results about data
+    // tslint:disable-next-line: no-console
+    //.then(emails => emails)
+    // tslint:disable-next-line: no-console
+    //.catch(e => console.log(e));
+  );
+
+async function Main(): Promise<void> {
+  await verifyAttachments().run();
+}
+
+const verifyAttachmentsActivity: AzureFunction = async (
+  context: Context
+): Promise<void> => {
+  context.log("start activity");
+  return await Main();
+};
+
+export default verifyAttachmentsActivity;
---
+++ commons/verify-sign/__tests__/wsaruba.test.ts
@@ -0,0 +1,43 @@
+import { right } from "fp-ts/lib/Either";
+import { task } from "fp-ts/lib/Task";
+import * as soap from "soap";
+import {
+  emailAttachmentsMock,
+  emailAttachmentsWithStatusMock
+} from "../../../__mocks__/mocks";
+import { urlDemoAruba } from "../../domain/data";
+import { IEmailAttachmentStatus } from "../../domain/models";
+import * as ArubaVerify from "../wsaruba";
+
+jest.mock("../../verify-sign/wsaruba", () => {
+  const originalWsAruba = jest.requireActual("../../verify-sign/wsaruba");
+  return {
+    __esModule: true,
+    ...originalWsAruba,
+    verify: jest.fn((email: IEmailAttachmentStatus) =>
+      task.of(emailAttachmentsWithStatusMock)
+    )
+  };
+});
+
+jest.mock("soap", () => {
+  const originalSoapModule = jest.requireActual("soap");
+  return {
+    __esModule: true,
+    ...originalSoapModule,
+    createClientAsync: jest.fn((urlWsd: string) => Promise.resolve({}))
+  };
+});
+
+describe("Connect to a wsdl Aruba Client and verify signature ", () => {
+  it("should connect to an imap server with the right credentials", async () => {
+    const wsFunc = await ArubaVerify.createClientAruba(urlDemoAruba).run();
+    expect(wsFunc.isRight()).toBeTruthy();
+  });
+
+  it("it should verify the attachment has signature ", async () => {
+    const verify = await ArubaVerify.verify(emailAttachmentsMock).run();
+    expect(verify).toBeDefined();
+    expect(verify).not.toBe(emailAttachmentsMock);
+  });
+});
